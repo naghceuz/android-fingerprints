5c8cf4b6e186aed9b957d390973ad7469ea6cad0
naghceuz <guanchenzhang@gmail.com>
2015-02-27 22:09:09 -0500

    Update a analyzeMutiply.py for more files

---
+++ v0.1.0/analyzeMutiply.py
@@ -0,0 +1,168 @@
+#analyze
+#and for methodcallsthesame, 
+#it doesnt help us unless we know what the values are
+import os
+import sys
+
+print "This is a python script to analyze the difference between two Andriod device(or emulator) finger-print \n"
+
+print str(len(sys.argv))
+
+
+# counters !
+# a function to count number of function calls in a file 
+def quickfunctioncounter(filename):
+	#quick counter function
+	countFile1 = 0
+	anotherfile1 = open(filename, 'rb')
+	while True:
+		buffer = anotherfile1.read(8192*1024)
+		if not buffer:
+			break
+		countFile1 += buffer.count('\n')
+	print str(countFile1) + " function calls"
+	anotherfile1.close()
+	return countFile1
+	# for line in anotherfile1:
+	# 	countFile1 =  countFile1+1
+	# print countFile1 
+	# anotherfile1.close()
+
+
+# input two files you want to comparsion
+thefile1 = str(sys.argv[1])
+thefile2 = str(sys.argv[2])
+
+
+
+
+
+
+print "The two files you input is:"
+
+print "File1: ", str(sys.argv[1])
+print "File2: ", str(sys.argv[2])
+
+
+file1N = open(thefile1, 'rb').readlines();
+file2N = open(thefile2, 'rb').readlines();
+
+
+def splitFuntionandValue(thefile, theNewFileName):
+	fileNew = open(theNewFileName,'w')
+	for line in thefile:
+		thecuttenline = line.split("=")
+		fileNew.write(thecuttenline[0]+ "\n")
+
+
+#fileNew.write(thecuttenline[0]+ "\n")
+#a file named file1function contains file1's all function
+splitFuntionandValue(file1N, "file1function.txt");
+
+#a file named file2function contains file2's all function
+splitFuntionandValue(file2N, "file2function.txt");
+
+
+thefile1function = open("file1function.txt", "rb").readlines()
+thefile2function = open("file2function.txt", "rb").readlines()
+
+#goal 1: same function same value
+# samefunctionsamevalue = open("same-function-same-value.txt", "w")
+#test 
+samefunctionsamevalue = open("same-function-same-value.txt", "w")
+
+samefunctiondifferentvalueValueinFile1 = open("samefunctiondifferentvaluethevalueinFile1.txt", "w")
+samefunctiondifferentvalueValueinFile2 = open("samefunctiondifferentvaluethevalueinFile2.txt", "w")
+
+#write comments for the same-function-same-value.txt file 
+samefunctionsamevalue.writelines("#This is the file which contain the same function with same value between file1 and file2 \n")
+samefunctionsamevalue.writelines("#The file1 is:"+ str(sys.argv[1]) +"\n")
+samefunctionsamevalue.writelines("#The file2 is:"+ str(sys.argv[2]) +"\n")
+
+
+#write comments for the samefunctiondifferentvaluethevalueinFile1.txt file 
+samefunctiondifferentvalueValueinFile1.writelines("#This is the file1 which contain the same function but different value between file1 and file2 ")
+samefunctiondifferentvalueValueinFile1.writelines("and the value is from file1 \n")
+
+#process the line1 and line2
+for line1 in file1N:
+	if line1[0] == "#": 
+		continue
+	elif line1 in file2N:
+		samefunctionsamevalue.write(line1);
+	else:
+		#goal 2: same function different value
+		line1count = line1.split("=")
+		line1count[0] += '\n'
+		if line1count[0] in thefile2function:
+			samefunctiondifferentvalueValueinFile1.write(line1)
+
+
+# samefunctiondifferentvalueValueinFile1
+# quickfunctioncounter()
+
+#goal 2: same function different value - branch for file2
+samefunctiondifferentvalueValueinFile2.writelines("#This is the file1 which contain the same function but different value between file1 and file2 ")
+samefunctiondifferentvalueValueinFile2.writelines("and the value is from file2 \n")
+
+for line2 in file2N:
+	if line2[0] == "#": 
+		continue
+	if line2 in file1N:
+		continue;
+	else:
+		#goal 2: same function different value
+		line2count = line2.split("=")
+		line2count[0] += '\n'
+		if line2count[0] in thefile1function:
+			samefunctiondifferentvalueValueinFile2.write(line2)
+
+
+
+#goal 3: different function , different value, and only in file1
+#functionOnlyinFile1.txt
+
+file3N = open("functionOnlyinFile1.txt", "w");
+file3N.writelines("#This file contains the unique functions only in file1 \n")
+
+
+for line3 in file1N:
+	if line3[0] == "#": 
+		continue
+	else:
+		theline3 = line3.split("=")
+		theline3[0] += '\n'
+		# for a in thefile2function:
+		if theline3[0] in thefile2function:
+			continue
+		else:
+			file3N.write(line3);
+
+
+#goal 4:different function , different value , and only in file2
+#functionOnlyinFile2.txt
+
+
+file4N = open("functionOnlyinFile2.txt", "w");
+file4N.writelines("#This file contains the unique functions only in file2 \n")
+
+
+for line4 in file2N:
+	if line4[0] == "#": 
+		continue
+	else:
+		theline4 = line4.split("=")
+		theline4[0] += '\n'
+		if theline4[0] in thefile1function:
+			continue
+		else:
+			file4N.write(line4);
+
+
+
+
+#remove the useless files
+os.remove("file1function.txt")
+os.remove("file2function.txt")
+
+
